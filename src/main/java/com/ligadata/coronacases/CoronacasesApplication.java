f0f8646806253faabe360b1bf4d72e603e23d14e
hala layous <hala.layous@apliman.net>
2021-07-11 15:52:14 +0300

    add context

--- src/main/java/com/ligadata/coronacases/CoronacasesApplication.java
+++ src/main/java/com/ligadata/coronacases/CoronacasesApplication.java
@@ -1,15 +1,22 @@
 package com.ligadata.coronacases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.StandardEnvironment;
 
-@SpringBootApplication( exclude = HibernateJpaAutoConfiguration.class,
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class,
         scanBasePackages = {"com.ligadata.coronacases"})
 public class CoronacasesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoronacasesApplication.class, args);
-	}
+    public static void main(String[] args) {
+       
+
+        
+        SpringApplication.run(CoronacasesApplication.class, args);
+    }
 
 }
